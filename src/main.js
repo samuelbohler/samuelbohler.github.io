ccbd3de34325f860529379cb9716750c798fe365
Samuel Bohler <samuel.bohler@gmail.com>
2025-06-20 20:07:13 -0500

    Adding project number

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@
 
 import { meet } from '@googleworkspace/meet-addons/meet.addons';
 
-const CLOUD_PROJECT_NUMBER = '989911054302';
+const CLOUD_PROJECT_NUMBER = '602584398177';
 const MAIN_STAGE_URL =
   'https://googleworkspace.github.io/meet/hello-world/MainStage.html';
 
